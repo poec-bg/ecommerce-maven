fd46ec89dbb5b15cc564696b95bc81fd03185fee
Raphael <pro.raphael.cartier@gmail.com>
2016-07-21 14:18:33 +0200

    changement BDD et singleton CommandeService

--- src/main/java/services/CommandeService.java
+++ src/main/java/services/CommandeService.java
@@ -1,7 +1,33 @@
 package services;
 
-/**
- * Created by formation on 21/07/2016.
- */
+import model.Commande;
+
 public class CommandeService {
+
+    /**
+     * Singleton
+     */
+    private static CommandeService instance;
+
+    /**
+     * Constructeur privé = personne ne peut faire de new ComandeService()
+     */
+    private CommandeService() {
+
+    }
+
+    /**
+     * Seule méthode pour récupérer une instance (toujours la même) de CommandeService
+     *
+     * @return toujours la même instance de CommandeService
+     */
+    public static CommandeService get() {
+        if (instance == null) {
+            instance = new CommandeService();
+        }
+        return instance;
+    }
+
+    
+
 }
