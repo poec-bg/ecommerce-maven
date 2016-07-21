d1c26a5908b7c43c4281d5bc79c12fb6c420d390
Souleymane Ndiaye <ndiaye.souleymane@live.fr>
2016-07-21 14:35:04 +0200

    Création de la méthode creerCommande dans la classe CommandeService

---
+++ src/main/java/services/CommandeService.java
@@ -0,0 +1,29 @@
+package services;
+
+import model.Commande;
+import model.Panier;
+import model.ProduitCommande;
+import model.ProduitPanier;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by formation on 21/07/2016.
+ */
+public class CommandeService {
+
+    public static Commande creer(Panier panier) {
+        Commande commande = new Commande();
+        commande.client = panier.client;
+        commande.date = new Date();
+        commande.produits = new ArrayList<>();
+        for (ProduitPanier produitPanier : panier.produits) {
+            commande.produits.add(new ProduitCommande(produitPanier.produit,
+                    produitPanier.quantite,
+                    produitPanier.produit.prixUnitaire));
+        }
+
+        return commande;
+    }
+}
