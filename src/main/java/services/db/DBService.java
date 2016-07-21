19608db35ad5eae40c77128653082c0b241e4ccc
lenaick <lenaickchartrain@gmail.com>
2016-07-21 21:46:49 +0200

    Modififcation de DBService pour qu'il soit opérationnel à la maison comme au centre M2I Formation

--- src/main/java/services/db/DBService.java
+++ src/main/java/services/db/DBService.java
@@ -1,8 +1,13 @@
 package services.db;
 
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Enumeration;
 
 public class DBService {
 
@@ -10,11 +15,24 @@ public class DBService {
     private Connection connection = null;
 
     private DBService() {
+        String adresseIpMysqlServer;
+        if(isMaison()){
+            adresseIpMysqlServer = "192.168.1.6";
+        }
+        else{
+            adresseIpMysqlServer = "10.110.10.13";
+        }
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://10.110.10.13/lenaick?user=root&password=formation&useSSL=false");
-        } catch (ClassNotFoundException | SQLException e) {
+            connection = DriverManager.getConnection("jdbc:mysql://" + adresseIpMysqlServer + "/lenaick?user=root&password=formation&useSSL=false");
+        } catch (SQLException sqle) {
+            System.out.println("Impossible d'établir de connection avec la base de données");
+            System.out.println("jdbc:mysql://" + adresseIpMysqlServer + "/lenaick?user=root&password=formation&useSSL=false");
+            System.out.println(sqle.getMessage());
             System.exit(0);
+        }
+        catch (ClassNotFoundException cnfe){
+            System.out.println(cnfe.getMessage());
         }
     }
 
@@ -37,5 +55,26 @@ public class DBService {
 
     public Connection getConnection() {
         return connection;
+    }
+
+    private boolean isMaison(){
+
+        try {
+            Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
+            while (e.hasMoreElements()){
+                Enumeration<InetAddress> i = e.nextElement().getInetAddresses();
+                while (i.hasMoreElements()){
+                    InetAddress a = i.nextElement();
+                    if(a.isSiteLocalAddress()){
+                        if(a.getHostAddress().toString().equals("192.168.1.1")){
+                            return true;
+                        }
+                    }
+                }
+            }
+        } catch (SocketException e1) {
+            e1.printStackTrace();
+        }
+        return false;
     }
 }
