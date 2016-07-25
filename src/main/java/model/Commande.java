e13a140eb8945cdff9959e8eaa46e13ee687f249
Nicolas Giard <nicogiard@gmail.com>
2016-07-25 10:09:27 +0200

    refactor: clean code

--- src/main/java/model/Commande.java
+++ src/main/java/model/Commande.java
@@ -3,7 +3,6 @@ package model;
 import org.joda.time.DateTime;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Commande {
@@ -18,10 +17,4 @@ public class Commande {
         produits = new ArrayList<>();
     }
 
-    public void affiche() {
-        System.out.println(String.format("Date : %s \nmodel.Client : %s \nAdresse : %s", date, client.nom, client.adressePostale));
-        for (ProduitCommande produitCommande : produits) {
-            System.out.println(String.format("model.Produit : %s x %d : %f", produitCommande.produit.nom, produitCommande.quantite, produitCommande.quantite * produitCommande.prixUnitaire));
-        }
-    }
 }
